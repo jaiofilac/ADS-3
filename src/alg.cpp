f17e400f1b954e0e920c03d613bb7a879d74ad1a
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-03-10 19:25:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+	int current = size - 1;
+	int result = 0;
+	while (true) {
+		if (value < arr[current]) {
+			current = current >> 1;
+		}
+		else if (value > arr[current]) {
+			current += current >> 1;
+		}
+		else if (value == arr[current]) {
+			for (; value == arr[current]; current++) {
+			}
+			current--;
+			for (; value == arr[current] && current >= 0; current--) {
+				result++;
+			}
+			break;
+		}
+		else if (current == 0)
+			break;
+	}
+	return result; 
 }
